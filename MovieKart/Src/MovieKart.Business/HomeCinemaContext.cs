347e253243e97426acebf4581526fa13f6d94d94
suchit khanna <khanna.suchit@gmail.com>
2017-05-18 16:40:32 +0530

    Adding Infrastrcuture Project.

---
+++ MovieKart/Src/MovieKart.Business/HomeCinemaContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MovieKart.Business.Configurations;
+using MovieKart.Domain;
+
+namespace MovieKart.Business
+{
+    public class HomeCinemaContext : DbContext
+    {
+        public HomeCinemaContext()
+            : base("HomeCinema")
+        {
+            Database.SetInitializer<HomeCinemaContext>(null);
+        }
+
+        public IDbSet<Customer> CustomerSet { get; set; }
+        public IDbSet<Movie> MovieSet { get; set; }
+        public IDbSet<Genre> GenreSet { get; set; }
+        public IDbSet<Stock> StockSet { get; set; }
+        public IDbSet<Rental> RentalSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new MovieConfiguration());
+            modelBuilder.Configurations.Add(new MovieConfiguration());
+            modelBuilder.Configurations.Add(new GenreConfiguration());
+            modelBuilder.Configurations.Add(new StockConfiguration());
+            modelBuilder.Configurations.Add(new RentalConfiguration());
+        }
+    }
+}
